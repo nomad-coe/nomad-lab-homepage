6a541593af462c9e26e5466bb2f0161dffb94c43
Markus Scheidgen <markus.scheidgen@gmail.com>
2023-02-05 15:52:24 +0100

    Added mobile menu.

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,8 @@
 import { MDCRipple } from '@material/ripple';
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCMenu } from '@material/menu';
+import { MDCDrawer } from "@material/drawer";
+import { MDCList } from "@material/list";
 
 const registerMenuEvents = (id: string) => {
     const element = document.querySelector('#' + id)
@@ -19,10 +21,40 @@ export function registerComponents() {
         new MDCRipple(button);
     });
 
+    document.querySelectorAll('.mdc-icon-button').forEach(button => {
+        new MDCRipple(button);
+    })
+
     new MDCTopAppBar(document.querySelector('.nomad-app-bar'));
 
     registerMenuEvents('services-menu')
     registerMenuEvents('documentation-menu')
     registerMenuEvents('support-menu')
     registerMenuEvents('about-menu')
+
+    const listEl = document.querySelector('.mdc-drawer .mdc-list-item');
+    const mainContentEl = document.querySelector('main');
+    const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+    const navButton = document.querySelector('.nomad-nav-button');
+
+    navButton.addEventListener('click', () => {
+        drawer.open = true;
+    })
+
+    listEl.addEventListener('click', () => {
+        drawer.open = false;
+    });
+
+
+    document.querySelectorAll('.mdc-list').forEach(listElement => {
+        new MDCList(listElement);
+    });
+
+    mainContentEl.addEventListener('click', () => {
+        drawer.open = false;
+    });
+
+    document.body.addEventListener('MDCDrawer:closed', () => {
+        (mainContentEl.querySelector('input, button') as HTMLElement).focus();
+    });
 }
