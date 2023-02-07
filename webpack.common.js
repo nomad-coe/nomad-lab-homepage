a5f53043bf9a52d0b84225f5be539b6c479c0279
Markus Scheidgen <markus.scheidgen@gmail.com>
2023-02-07 16:53:43 +0100

    More content.

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const loremIpsum = require("lorem-ipsum").loremIpsum;
 
-const pages = ["index", "nomad", "nomad-oasis", "help"];
+const pages = ["index", "nomad", "nomad-oasis", "help", "tutorials", "installations", "source-code"];
 
 module.exports = {
   entry: pages.reduce((config, page) => {
